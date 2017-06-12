d3d0358b75af4cdcead311251d5171318eba3795
Russ Rinzler <russ.rinzler@gmail.com>
2017-06-11 23:14:31 -0700

    load redux

---
+++ src/ColorInteractionsApp.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import 'normalize.css';
+import './main.scss';
+
+export default class ColorInteractionsApp extends Component {
+    render() {
+        return <p>I am the app!</p>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ if (module.hot) {
     module.hot.accept();
 }
 
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import ColorInteractionsApp from './ColorInteractionsApp';
+
+const store = createStore(placeholderReducer);
+
 import 'normalize.css';
 import './main.scss';
 
-import React from 'react';
-import {render} from 'react-dom';
+render(
+    <Provider store={store}>
+        <ColorInteractionsApp />
+    </Provider>,
+    document.getElementById('app')
+);
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+function placeholderReducer(state = [], action) {
+    switch (action.type) {
+    default:
+        return state
+    }
 }
-
-render(<App/>, document.getElementById('app'));
