fb54571ae724275d17c791239bbab8362b6688d5
Russ Rinzler <russ.rinzler@gmail.com>
2017-07-16 16:33:23 -0700

    add a home link component

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import HomeLink from '../HomeLink';
 import Menu from '../Menu';
+
+import './home.scss';
 
 export default function Home() {
     return (
-        <div className="full-screen position-relative">
+        <div className="full-screen position-relative display-flex home">
             <Menu>
                 hay
             </Menu>
-            Home!
+            <HomeLink />
         </div>
     );
 }
---
+++ src/components/HomeLink/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Menu from '../Menu';
+
+import './home_link.scss';
+
+export default function HomeLink() {
+    return (
+        <a href="#" className="homeLink">
+            HomeLink
+        </a>
+    );
+}
