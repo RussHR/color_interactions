ae9fc654257fca5b0cfe3f1c3f31d990c74ceda1
Russ Rinzler <russ.rinzler@gmail.com>
2017-08-14 23:29:41 -0700

    add a thumbnail for gradients on the home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { pick } from 'lodash';
+import { pick, times } from 'lodash';
 import HomeLink from '../HomeLink';
 import Menu from '../Menu';
 
@@ -11,6 +11,18 @@ function mapStateToProps(state) {
 }
 
 let Home = ({ colors: { color0, color1, color2 } }) => {
+    const gradientBars = times(5, (i) => {
+        let style;
+        if (i % 2 == 0) {
+            style = { width: '20%', background: `linear-gradient(rgb(${color0.r}, ${color0.g}, ${color0.b}), rgb(${color1.r}, ${color1.g}, ${color1.b}))`};
+        } else {
+            style = { width: '20%', backgroundColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})` };
+        }
+        return (
+            <div key={i} style={style} />
+        );
+    });
+
     return (
         <div className="full-screen position-relative display-flex home">
             <Menu>
@@ -57,18 +69,8 @@ let Home = ({ colors: { color0, color1, color2 } }) => {
             </HomeLink>
 
             <HomeLink href="#gradient" title="gradient">
-                <div
-                    className="homeLink__thumbnail position-relative"
-                    style={{ backgroundColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})` }}
-                >
-                    <div
-                        className="LighterAndOrDarker__leftBlock position-absolute LighterAndOrDarker__block--thumbnail"
-                        style={{ backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` }}
-                    />
-                    <div
-                        className="LighterAndOrDarker__rightBlock position-absolute LighterAndOrDarker__block--thumbnail"
-                        style={{ backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` }}
-                    />
+                <div className="homeLink__thumbnail display-flex">
+                    {gradientBars}
                 </div>
             </HomeLink>
         </div>
