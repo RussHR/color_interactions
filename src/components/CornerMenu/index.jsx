a97d439eca9097e60dfeb48fcef4a45a0d4d65b1
Russ Rinzler <russ.rinzler@gmail.com>
2017-08-24 20:12:34 -0700

    allow two of the colors to be changed from the home screen

--- src/components/CornerMenu/index.jsx
+++ src/components/CornerMenu/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pick } from 'lodash';
+import { ChromePicker } from 'react-color';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { CHANGE_COLOR, RANDOMIZE_COLORS } from '../../constants/actionTypes';
@@ -15,7 +16,11 @@ class CornerMenu extends Component {
 
     constructor(props){
         super(props);
-        this.state = { isOpen: false, isHidden: false };
+        this.state = {
+            isOpen: false,
+            isHidden: false,
+            activeColor: 'color0'
+        };
         this.toggleOpen = this.toggleOpen.bind(this);
         this.handleKeydown = this.handleKeydown.bind(this);
     }
@@ -48,25 +53,72 @@ class CornerMenu extends Component {
         this.setState({ isOpen: !this.state.isOpen });
     }
 
+    /**
+     * Changes which color is shown in the color picker.
+     * @param {string} activeColor - the color (e.g. color0, color1) to show in the color picker
+     * @returns {void}
+     */
+    changeActiveColor(activeColor) {
+        this.setState({ activeColor });
+    }
+
+    /**
+     * Calls dispatch to change a color.
+     * @param {object} payload - data for colors to be changed including new color in rgb format
+     * @returns {void}
+     */
+    changeColor(payload) {
+        this.props.dispatch({
+            type: CHANGE_COLOR,
+            payload
+        });
+    }
+
     render() {
         const menuClasses = classNames('CornerMenu', 'position-absolute', 'top-0', 'right-0', { 'hidden': this.state.isHidden });
         let menuContent;
 
-        if (this.state.isOpen) {
-            menuContent = (
+        if (!this.state.isOpen) {
+            return (
+                <div className={menuClasses}>
+                    <button className="CornerMenu__cornerButton no-padding" onClick={this.toggleOpen}>menu</button>
+                </div>
+            );
+        }
+
+        const { colors } = this.props;
+        const { activeColor } = this.state;
+
+        return (
+            <div className={menuClasses}>
                 <div>
                     <button aria-label="Close" className="CornerMenu__close position-absolute top-0 right-0" onClick={this.toggleOpen}>
                         X
                     </button>
+                    <label htmlFor="color0">Color 1</label>
+                    <input
+                        type="radio"
+                        name="color"
+                        id="color0"
+                        checked={activeColor == 'color0'}
+                        onChange={() => this.changeActiveColor('color0')}
+                    />
+                    <br />
+                    <label htmlFor="color1">Color 2</label>
+                    <input
+                        type="radio"
+                        name="color"
+                        id="color1"
+                        checked={activeColor == 'color1'}
+                        onChange={() => this.changeActiveColor('color1')}
+                    />
+                    <br />
+                    <ChromePicker
+                        disableAlpha={true}
+                        color={colors[activeColor]}
+                        onChange={({ rgb: { r, g, b } }) => this.changeColor({ [`${activeColor}`]: { r, g, b } })}
+                    />
                 </div>
-            );
-        } else {
-            menuContent = <button className="CornerMenu__cornerButton no-padding" onClick={this.toggleOpen}>menu</button>;
-        }
-
-        return (
-            <div className={menuClasses}>
-                {menuContent}
             </div>
         );
     }
