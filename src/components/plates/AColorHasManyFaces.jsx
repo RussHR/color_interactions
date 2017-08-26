9d09a3d802f1194132bb3d07929c7873d0579874
Russ Rinzler <russ.rinzler@gmail.com>
2017-08-26 15:14:49 +0100

    make AColorHasManyFaces compatible with CornerMenu

--- src/components/plates/AColorHasManyFaces.jsx
+++ src/components/plates/AColorHasManyFaces.jsx
@@ -12,179 +12,30 @@ function mapStateToProps(state) {
     return pick(state, 'colors');
 }
 
-class AColorHasManyFaces extends Component {
-    constructor(props) {
-        super(props);
+function AColorHasManyFaces({ colors }) {
+    const { color0, color1, color2 } = colors;
+    const leftPlateColor = { backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` };
+    const rightPlateColor = { backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` };
+    const innerPlateColor = { backgroundColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})` };
 
-        this.state = {
-            activeColor: 'color0',
-            lockedColors: {
-                color0: false,
-                color1: false,
-                color2: false
-            }
-        };
-        this.randomizeColorsViaKeyboard = this.randomizeColorsViaKeyboard.bind(this);
-    }
-
-    componentDidMount() {
-        window.document.addEventListener('keydown', this.randomizeColorsViaKeyboard);
-    }
-
-    componentWillUnmount() {
-        window.document.removeEventListener('keydown', this.randomizeColorsViaKeyboard);
-    }
-
-    /**
-     * Randomizes colors with keyboard input.
-     * @param {object} e - DOM event with keyCode property
-     * @returns {void}
-     */
-    randomizeColorsViaKeyboard({ keyCode }) {
-        // if key is 'r'
-        if (keyCode === 82) {
-            this.randomizeColors();
-        }
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    changeColor(payload) {
-        this.props.dispatch({
-            type: CHANGE_COLOR,
-            payload
-        });
-    }
-
-    changeActiveColor(activeColor) {
-        this.setState({ activeColor });
-    }
-
-    /**
-     * Toggles the state of whether a color is "locked", i.e. prevented, from changing into a random color.
-     * @param {string} color - string that determines which color is locked, e.g. 'color1'
-     * @returns {void}
-     */
-    toggleLockedColor(color) {
-        const lockedColors = assign({}, this.state.lockedColors, { [`${color}`]: !this.state.lockedColors[color] });
-        this.setState({ lockedColors });
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    randomizeColors() {
-        this.props.dispatch({ type: RANDOMIZE_COLORS, payload: { lockedColors: this.state.lockedColors } });
-    }
-
-    render() {
-        const { color0, color1, color2 } = this.props.colors;
-        const { colors } = this.props;
-        const { activeColor, lockedColors } = this.state;
-        const leftPlateColor = { backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` };
-        const rightPlateColor = { backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` };
-        const innerPlateColor = { backgroundColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})` };
-
-        return (
-            <div className="full-screen display-flex">
-                <CornerMenu>
-                    <label htmlFor="color0">Left Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color0"
-                        checked={activeColor == 'color0'}
-                        onChange={() => this.changeActiveColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1">Right Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color1"
-                        checked={activeColor == 'color1'}
-                        onChange={() => this.changeActiveColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2">Inner Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color2"
-                        checked={activeColor == 'color2'}
-                        onChange={() => this.changeActiveColor('color2')}
-                    />
-                    <br/>
-                    <br />
-                    <ChromePicker
-                        disableAlpha={true}
-                        color={colors[activeColor]}
-                        onChange={({ rgb: { r, g, b } }) => this.changeColor({ [`${activeColor}`]: { r, g, b } })}
-                    />
-                    <br/>
-                    <button onClick={() => this.randomizeColors()}>randomizeColors</button>
-                    <br />
-                    <label htmlFor="color0-lock">Lock Left Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color0-lock"
-                        value="color0"
-                        checked={lockedColors['color0']}
-                        onChange={() => this.toggleLockedColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1-lock">Lock Right Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color1-lock"
-                        value="color1"
-                        checked={lockedColors['color1']}
-                        onChange={() => this.toggleLockedColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2-lock">Lock Inner Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color2-lock"
-                        value="color2"
-                        checked={lockedColors['color2']}
-                        onChange={() => this.toggleLockedColor('color2')}
-                    />
-                    <br />
-                    <a href="#">Home</a>
-                </CornerMenu>
-                <div
-                    className="justify-content-center align-items-center display-flex half-width full-height"
-                    style={leftPlateColor}
-                >
-                    <div className="aColorHasManyFaces__innerBlock" style={innerPlateColor}></div>
-                </div>
-                <div
-                    className="justify-content-center align-items-center display-flex half-width full-height"
-                    style={rightPlateColor}
-                >
-                    <div className="aColorHasManyFaces__innerBlock" style={innerPlateColor}></div>
-                </div>
+    return (
+        <div className="full-screen display-flex">
+            <CornerMenu colorLabels={['left background color', 'right background color', 'inner color']} />
+            <div
+                className="justify-content-center align-items-center display-flex half-width full-height"
+                style={leftPlateColor}
+            >
+                <div className="aColorHasManyFaces__innerBlock" style={innerPlateColor}></div>
             </div>
-        );
-    }
+            <div
+                className="justify-content-center align-items-center display-flex half-width full-height"
+                style={rightPlateColor}
+            >
+                <div className="aColorHasManyFaces__innerBlock" style={innerPlateColor}></div>
+            </div>
+        </div>
+    );
 }
-
-AColorHasManyFaces.defaultProps = {
-    colors: {
-        color0: generateRandomColor(),
-        color1: generateRandomColor(),
-        color2: generateRandomColor()
-    }
-};
 
 AColorHasManyFaces.propTypes = {
     colors: (props, propName, componentName) => {
