d2231b2d19edb47bd7769ca2e9df993062f93a24
Russ Rinzler <russ.rinzler@gmail.com>
2017-08-26 15:10:13 +0100

    make lighter-and-or-darker compatible with corner menu

--- src/components/plates/FalseGradient.jsx
+++ src/components/plates/FalseGradient.jsx
@@ -15,71 +15,9 @@ class FalseGradient extends Component {
         super(props);
 
         this.state = {
-            activeColor: 'color0',
-            lockedColors: {
-                color0: false,
-                color1: false,
-                color2: false
-            },
             numBars: 5
         };
-        this.randomizeColorsViaKeyboard = this.randomizeColorsViaKeyboard.bind(this);
         this.changeNumBars = this.changeNumBars.bind(this);
-    }
-
-    componentDidMount() {
-        window.document.addEventListener('keydown', this.randomizeColorsViaKeyboard);
-    }
-
-    componentWillUnmount() {
-        window.document.removeEventListener('keydown', this.randomizeColorsViaKeyboard);
-    }
-
-    /**
-     * Randomizes colors with keyboard input.
-     * @param {object} e - DOM event with keyCode property
-     * @returns {void}
-     */
-    randomizeColorsViaKeyboard({ keyCode }) {
-        // if key is 'r'
-        if (keyCode === 82) {
-            this.randomizeColors();
-        }
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    changeColor(payload) {
-        this.props.dispatch({
-            type: CHANGE_COLOR,
-            payload
-        });
-    }
-
-    changeActiveColor(activeColor) {
-        this.setState({ activeColor });
-    }
-
-    /**
-     * Toggles the state of whether a color is "locked", i.e. prevented, from changing into a random color.
-     * @param {string} color - string that determines which color is locked, e.g. 'color1'
-     * @returns {void}
-     */
-    toggleLockedColor(color) {
-        const lockedColors = assign({}, this.state.lockedColors, { [`${color}`]: !this.state.lockedColors[color] });
-        this.setState({ lockedColors });
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    randomizeColors() {
-        this.props.dispatch({ type: RANDOMIZE_COLORS, payload: { lockedColors: this.state.lockedColors } });
     }
 
     changeNumBars({ currentTarget: { value: numBars } }) {
@@ -88,10 +26,8 @@ class FalseGradient extends Component {
 
     render() {
         const { color0, color1, color2 } = this.props.colors;
-        const { colors } = this.props;
-        const { activeColor, lockedColors, numBars } = this.state;
-        const leftPlateColor = { backgroundColor: `rgb()` };
-        const rightPlateColor = { backgroundColor: `rgb()` };
+        const { numBars } = this.state;
+
         const bars = times(numBars, (i) => {
             let style;
             if (i % 2 == 0) {
@@ -106,73 +42,7 @@ class FalseGradient extends Component {
 
         return (
             <div className="full-screen display-flex">
-                <CornerMenu>
-                    <label htmlFor="color0">Left Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color0"
-                        checked={activeColor == 'color0'}
-                        onChange={() => this.changeActiveColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1">Right Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color1"
-                        checked={activeColor == 'color1'}
-                        onChange={() => this.changeActiveColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2">Inner Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color2"
-                        checked={activeColor == 'color2'}
-                        onChange={() => this.changeActiveColor('color2')}
-                    />
-                    <br/>
-                    <br />
-                    <ChromePicker
-                        disableAlpha={true}
-                        color={colors[activeColor]}
-                        onChange={({ rgb: { r, g, b } }) => this.changeColor({ [`${activeColor}`]: { r, g, b } })}
-                    />
-                    <br/>
-                    <button onClick={() => this.randomizeColors()}>randomizeColors</button>
-                    <br />
-                    <label htmlFor="color0-lock">Lock Left Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color0-lock"
-                        value="color0"
-                        checked={lockedColors['color0']}
-                        onChange={() => this.toggleLockedColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1-lock">Lock Right Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color1-lock"
-                        value="color1"
-                        checked={lockedColors['color1']}
-                        onChange={() => this.toggleLockedColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2-lock">Lock Inner Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color2-lock"
-                        value="color2"
-                        checked={lockedColors['color2']}
-                        onChange={() => this.toggleLockedColor('color2')}
-                    />
-                    <br />
+                <CornerMenu colorLabels={['gradient color 1', 'gradient color 2', 'solid color']}>
                     <label htmlFor="num-bars">number of bars: </label>
                     <input
                         id="num-bars"
@@ -182,7 +52,7 @@ class FalseGradient extends Component {
                         value={numBars}
                         onChange={this.changeNumBars}
                     />
-                    <a href="#">Home</a>
+                    <br />
                 </CornerMenu>
 
                 {bars}
@@ -190,14 +60,6 @@ class FalseGradient extends Component {
         );
     }
 }
-
-FalseGradient.defaultProps = {
-    colors: {
-        color0: generateRandomColor(),
-        color1: generateRandomColor(),
-        color2: generateRandomColor()
-    }
-};
 
 FalseGradient.propTypes = {
     colors: (props, propName, componentName) => {
--- src/components/plates/LighterAndOrDarker.jsx
+++ src/components/plates/LighterAndOrDarker.jsx
@@ -18,74 +18,19 @@ class LighterAndOrDarker extends Component {
         super(props);
 
         this.state = {
-            activeColor: 'color0',
-            lockedColors: {
-                color0: false,
-                color1: false,
-                color2: false
-            },
             squaresApart: false
         };
-        this.randomizeColorsViaKeyboard = this.randomizeColorsViaKeyboard.bind(this);
+
         this.toggleTouchingSquares = this.toggleTouchingSquares.bind(this);
         this.toggleTouchingSquaresViaKeyboard = this.toggleTouchingSquaresViaKeyboard.bind(this);
     }
 
     componentDidMount() {
-        window.document.addEventListener('keydown', this.randomizeColorsViaKeyboard);
         window.document.addEventListener('keydown', this.toggleTouchingSquaresViaKeyboard);
     }
 
     componentWillUnmount() {
-        window.document.removeEventListener('keydown', this.randomizeColorsViaKeyboard);
         window.document.removeEventListener('keydown', this.toggleTouchingSquaresViaKeyboard);
-    }
-
-    /**
-     * Randomizes colors with keyboard input.
-     * @param {object} e - DOM event with keyCode property
-     * @returns {void}
-     */
-    randomizeColorsViaKeyboard({ keyCode }) {
-        // if key is 'r'
-        if (keyCode === 82) {
-            this.randomizeColors();
-        }
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    changeColor(payload) {
-        this.props.dispatch({
-            type: CHANGE_COLOR,
-            payload
-        });
-    }
-
-    changeActiveColor(activeColor) {
-        this.setState({ activeColor });
-    }
-
-    /**
-     * Toggles the state of whether a color is "locked", i.e. prevented, from changing into a random color.
-     * @param {string} color - string that determines which color is locked, e.g. 'color1'
-     * @returns {void}
-     */
-    toggleLockedColor(color) {
-        const lockedColors = assign({}, this.state.lockedColors, { [`${color}`]: !this.state.lockedColors[color] });
-        this.setState({ lockedColors });
-    }
-
-    /**
-     * Calls dispatch to change a color.
-     * @param {object} payload - data for colors to be changed including new color in rgb format
-     * @returns {void}
-     */
-    randomizeColors() {
-        this.props.dispatch({ type: RANDOMIZE_COLORS, payload: { lockedColors: this.state.lockedColors } });
     }
 
     /**
@@ -111,9 +56,9 @@ class LighterAndOrDarker extends Component {
         const { color0, color1, color2 } = this.props.colors;
         const { colors } = this.props;
         const { activeColor, lockedColors, squaresApart } = this.state;
-        const leftPlateColor = { backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` };
-        const rightPlateColor = { backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` };
-        const rightPlateClasses = classNames(
+        const leftBlockColor = { backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` };
+        const rightBlockColor = { backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` };
+        const rightBlockClasses = classNames(
             'LighterAndOrDarker__rightBlock',
             'position-absolute',
             { 'LighterAndOrDarker__rightBlock--apart': squaresApart }
@@ -122,91 +67,16 @@ class LighterAndOrDarker extends Component {
 
         return (
             <div className="full-screen position-relative" style={bgColor}>
-                <CornerMenu>
-                    <label htmlFor="color0">Left Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color0"
-                        checked={activeColor == 'color0'}
-                        onChange={() => this.changeActiveColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1">Right Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color1"
-                        checked={activeColor == 'color1'}
-                        onChange={() => this.changeActiveColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2">Background Color</label>
-                    <input
-                        type="radio"
-                        name="color"
-                        id="color2"
-                        checked={activeColor == 'color2'}
-                        onChange={() => this.changeActiveColor('color2')}
-                    />
-                    <br/>
-                    <br />
-                    <ChromePicker
-                        disableAlpha={true}
-                        color={colors[activeColor]}
-                        onChange={({ rgb: { r, g, b } }) => this.changeColor({ [`${activeColor}`]: { r, g, b } })}
-                    />
-                    <br/>
-                    <button onClick={() => this.randomizeColors()}>randomizeColors</button>
-                    <br />
-                    <label htmlFor="color0-lock">Lock Left Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color0-lock"
-                        value="color0"
-                        checked={lockedColors['color0']}
-                        onChange={() => this.toggleLockedColor('color0')}
-                    />
-                    <br />
-                    <label htmlFor="color1-lock">Lock Right Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color1-lock"
-                        value="color1"
-                        checked={lockedColors['color1']}
-                        onChange={() => this.toggleLockedColor('color1')}
-                    />
-                    <br />
-                    <label htmlFor="color2-lock">Lock Background Color</label>
-                    <input
-                        type="checkbox"
-                        name="color"
-                        id="color2-lock"
-                        value="color2"
-                        checked={lockedColors['color2']}
-                        onChange={() => this.toggleLockedColor('color2')}
-                    />
-                    <br />
+                <CornerMenu colorLabels={['left block color', 'right block color', 'background color']}>
                     <button onClick={this.toggleTouchingSquares}>separate squares (S)</button>
                     <br/>
-                    <a href="#">Home</a>
                 </CornerMenu>
-                <div className="LighterAndOrDarker__leftBlock position-absolute" style={leftPlateColor} />
-                <div className={rightPlateClasses} style={rightPlateColor} />
+                <div className="LighterAndOrDarker__leftBlock position-absolute" style={leftBlockColor} />
+                <div className={rightBlockClasses} style={rightBlockColor} />
             </div>
         );
     }
 }
-
-LighterAndOrDarker.defaultProps = {
-    colors: {
-        color0: generateRandomColor(),
-        color1: generateRandomColor(),
-        color2: generateRandomColor()
-    }
-};
 
 LighterAndOrDarker.propTypes = {
     colors: (props, propName, componentName) => {
