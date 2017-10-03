0db4c611dc570c1d69ff7b96b7a6c81c6f2c6c16
Russ Rinzler <russ.rinzler@gmail.com>
2017-10-03 12:34:52 -1000

    allow bezold blocks to touch

--- src/components/plates/BezoldEffect.jsx
+++ src/components/plates/BezoldEffect.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { assign, pick } from 'lodash';
 import { validatePropColors } from '../../helpers/colorHelpers';
 import CornerMenu from '../CornerMenu';
+import classNames from 'classnames';
 
 import './bezold_effect.scss';
 
@@ -15,19 +16,21 @@ class BezoldEffect extends Component {
         super(props);
 
         this.state = {
-            customMortarColors: false
+            customMortarColors: false,
+            blocksTouching: false
         };
 
         this.toggleCustomMortarColors = this.toggleCustomMortarColors.bind(this);
-        this.toggleCustomMortarColorsViaKeyboard = this.toggleCustomMortarColorsViaKeyboard.bind(this);
+        this.toggleBlocksTouching = this.toggleBlocksTouching.bind(this);
+        this.toggleBlocksTouchingViaKeyboard = this.toggleBlocksTouchingViaKeyboard.bind(this);
     }
 
     componentDidMount() {
-        window.document.addEventListener('keydown', this.toggleCustomMortarColorsViaKeyboard);
+        window.document.addEventListener('keydown', this.toggleBlocksTouchingViaKeyboard);
     }
 
     componentWillUnmount() {
-        window.document.removeEventListener('keydown', this.toggleCustomMortarColorsViaKeyboard);
+        window.document.removeEventListener('keydown', this.toggleBlocksTouchingViaKeyboard);
     }
 
     /**
@@ -42,17 +45,25 @@ class BezoldEffect extends Component {
      * Toggles the state of whether mortar colors are set by user.
      * @returns {void}
      */
-    toggleCustomMortarColorsViaKeyboard({ keyCode }) {
+    toggleBlocksTouchingViaKeyboard({ keyCode }) {
         // if key is 'k'
         if (keyCode === 75) {
-            this.toggleCustomMortarColors();
+            this.toggleBlocksTouching();
         }
+    }
+
+    /**
+     * Toggle whether the blocks are touching.
+     * @returns {void}
+     */
+    toggleBlocksTouching() {
+        this.setState({ blocksTouching: !this.state.blocksTouching });
     }
 
     render() {
         const { colors } = this.props;
         const { color0, color1, color2 } = colors;
-        const { customMortarColors } = this.state;
+        const { customMortarColors, blocksTouching } = this.state;
 
         const colorLabels = customMortarColors
             ? ['brick color', 'mortar color 1', 'mortar color 2']
@@ -71,8 +82,17 @@ class BezoldEffect extends Component {
                     linear-gradient(155deg, rgb(${color0.r}, ${color0.g}, ${color0.b}) 10px, transparent 10px)`
         };
 
-        const leftBlockStyle = assign({ backgroundColor: customMortarColors ? `rgb(${color1.r}, ${color1.g}, ${color1.b})` : 'white' }, commonStyle);
-        const rightBlockStyle = assign({ backgroundColor: customMortarColors ? `rgb(${color2.r}, ${color2.g}, ${color2.b})` : 'black' }, commonStyle);
+        const leftBlockStyle = assign({
+            backgroundColor: customMortarColors ? `rgb(${color1.r}, ${color1.g}, ${color1.b})` : 'white'
+        }, commonStyle);
+
+        const rightBlockStyle = assign({
+            backgroundColor: customMortarColors ? `rgb(${color2.r}, ${color2.g}, ${color2.b})` : 'black'
+        }, commonStyle);
+
+        const brickClassNames = classNames('BezoldEffect__bricks', 'standard-transition', {
+            'BezoldEffect__bricks--touching': blocksTouching
+        });
 
         return (
             <div className="full-screen display-flex justify-content-space-evenly align-items-center">
@@ -84,10 +104,12 @@ class BezoldEffect extends Component {
                         onChange={this.toggleCustomMortarColors}
                         checked={customMortarColors}
                     />
+                    <br />
+                    <button onClick={this.toggleBlocksTouching}>touch blocks (k)</button>
                     <br/>
                 </CornerMenu>
-                <div className="BezoldEffect__bricks" style={leftBlockStyle} />
-                <div className="BezoldEffect__bricks" style={rightBlockStyle} />
+                <div className={brickClassNames} style={leftBlockStyle} />
+                <div className={brickClassNames} style={rightBlockStyle} />
             </div>
         );
     }
