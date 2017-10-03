67bb3810aeddaae741eb55bb40e83c0c808a7bbe
Russ Rinzler <russ.rinzler@gmail.com>
2017-10-03 13:54:06 -1000

    convert ColorIntervals to a component to support storage of color modifiers

--- src/components/plates/ColorIntervals.jsx
+++ src/components/plates/ColorIntervals.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { pick } from 'lodash';
 import { validatePropColors, getBetweenColor } from '../../helpers/colorHelpers';
@@ -8,31 +8,33 @@ function mapStateToProps(state) {
     return pick(state, 'colors');
 }
 
-let ColorIntervals = ({ colors }) => {
-    const { color0, color1 } = colors;
+class ColorIntervals extends Component {
+    render() {
+        const { color0, color1 } = this.props.colors;
 
-    return (
-        <div className="full-screen">
-            <CornerMenu colorLabels={['outer base color', 'inner base color']} />
+        return (
+            <div className="full-screen">
+                <CornerMenu colorLabels={['outer base color', 'inner base color']} />
 
-            <div className="display-inline-block half-height half-width position-relative">
-                <div className="position-absolute right-0 bottom-0 half-height half-width" />
+                <div className="display-inline-block half-height half-width position-relative">
+                    <div className="position-absolute right-0 bottom-0 half-height half-width" />
+                </div>
+
+                <div className="display-inline-block half-height half-width position-relative">
+                    <div className="position-absolute bottom-0 left-0 half-height half-width" />
+                </div>
+
+                <div className="display-inline-block half-height half-width position-relative">
+                    <div className="position-absolute top-0 right-0 half-height half-width" />
+                </div>
+
+                <div className="display-inline-block half-height half-width position-relative">
+                    <div className="position-absolute top-0 left-0 half-height half-width" />
+                </div>
             </div>
-
-            <div className="display-inline-block half-height half-width position-relative">
-                <div className="position-absolute bottom-0 left-0 half-height half-width" />
-            </div>
-
-            <div className="display-inline-block half-height half-width position-relative">
-                <div className="position-absolute top-0 right-0 half-height half-width" />
-            </div>
-
-            <div className="display-inline-block half-height half-width position-relative">
-                <div className="position-absolute top-0 left-0 half-height half-width" />
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 ColorIntervals.propTypes = {
     colors: validatePropColors(2)
