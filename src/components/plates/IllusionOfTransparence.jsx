5e582f0ebcd941c83d58f7aa09d819183d047706
Russ Rinzler <russ.rinzler@gmail.com>
2017-10-12 21:20:14 -0700

    allow overlapping color of IllusionOfTransparence to be set manually again

--- src/components/plates/IllusionOfTransparence.jsx
+++ src/components/plates/IllusionOfTransparence.jsx
@@ -11,22 +11,59 @@ function mapStateToProps(state) {
 }
 
 class IllusionOfTransparence extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            setOverlappingColor: false
+        };
+
+        this.toggleSetOverlappingColor = this.toggleSetOverlappingColor.bind(this);
+    }
+
+    /**
+     * Toggles the state of whether the middle color is set to color2.
+     * @returns {void}
+     */
+    toggleSetOverlappingColor() {
+        this.setState({ setOverlappingColor: !this.state.setOverlappingColor });
+    }
+
     render() {
         const { color0, color1 } = this.props.colors;
         const leftBlockStyle = { backgroundColor: `rgb(${color0.r}, ${color0.g}, ${color0.b})` };
         const rightBlockStyle = { backgroundColor: `rgb(${color1.r}, ${color1.g}, ${color1.b})` };
-        const overlappingPartColor = {
-            r: Math.round((color0.r * 0.5) + color1.r * 0.5),
-            g: Math.round((color0.g * 0.5) + color1.g * 0.5),
-            b: Math.round((color0.b * 0.5) + color1.b * 0.5)
-        };
-        const overlappingPartStyle = {
-            backgroundColor: `rgb(${overlappingPartColor.r}, ${overlappingPartColor.g}, ${overlappingPartColor.b})`
-        };
+
+        let overlappingPartStyle;
+        let colorLabels = ['left color', 'right color'];
+
+        if (this.state.setOverlappingColor) {
+            const { color2 } = this.props.colors;
+            overlappingPartStyle = { backgroundColor: `rgb(${color2.r}, ${color2.g}, ${color2.b})` };
+            colorLabels.push('overlapping color');
+        } else {
+            const overlappingPartColor = {
+                r: Math.round((color0.r * 0.5) + color1.r * 0.5),
+                g: Math.round((color0.g * 0.5) + color1.g * 0.5),
+                b: Math.round((color0.b * 0.5) + color1.b * 0.5)
+            };
+            overlappingPartStyle = {
+                backgroundColor: `rgb(${overlappingPartColor.r}, ${overlappingPartColor.g}, ${overlappingPartColor.b})`
+            };
+        }
 
         return (
             <div className="full-screen">
-                <CornerMenu colorLabels={['left color', 'right color', 'overlapping color']} />
+                <CornerMenu colorLabels={colorLabels}>
+                    <label htmlFor="custom-overlapping-color">custom overlapping color: </label>
+                    <input
+                        id="custom-overlapping-color"
+                        type="checkbox"
+                        onChange={this.toggleSetOverlappingColor}
+                        checked={this.state.setOverlappingColor}
+                    />
+                    <br />
+                </CornerMenu>
                 <div className="half-width full-height display-inline-block position-relative">
                     <div className="position-absolute IllusionOfTransparence__leftBlock" style={leftBlockStyle}>
                         <div
