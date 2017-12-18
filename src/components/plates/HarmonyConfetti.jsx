ee8ce96063440a3f527fe15cb0f8e782b957144f
Russ Rinzler <russ.rinzler@gmail.com>
2017-12-17 21:22:14 -0800

    somehow get four bars of confetti displayed

--- src/components/plates/HarmonyConfetti.jsx
+++ src/components/plates/HarmonyConfetti.jsx
@@ -18,7 +18,7 @@ class HarmonyConfetti extends Component {
         this.noise = new Noise(Math.random());
         this.animate = this.animate.bind(this);
         this.state = {};
-        this.state.smallBoxPositions = this.randomSmallBoxPositions();
+        this.state.smallBoxProperties = this.randomSmallBoxProperties();
 
         this.randomizeSmallBoxes = this.randomizeSmallBoxes.bind(this);
         this.randomizeSmallBoxesViaKeyboard = this.randomizeSmallBoxesViaKeyboard.bind(this);
@@ -47,23 +47,23 @@ class HarmonyConfetti extends Component {
     }
 
     randomizeSmallBoxes() {
-        this.setState({ smallBoxPositions: this.randomSmallBoxPositions() });
+        this.setState({ smallBoxProperties: this.randomSmallBoxProperties() });
     }
 
-    randomSmallBoxPositions() {
+    randomSmallBoxProperties() {
         return times(90, i => {
             const { color1, color2, color3, color4 } = this.props.colors;
             const colorRoll = Math.random();
             let backgroundColor;
 
             if (colorRoll < 0.125) {
-                backgroundColor = `rgb(${color2.r}, ${color2.g}, ${color2.b})`;
+                backgroundColor = 2;
             } else if (colorRoll < 0.375) {
-                backgroundColor = `rgb(${color3.r}, ${color3.g}, ${color3.b})`;            
+                backgroundColor = 3;
             } else if (colorRoll < 0.475) {
-                backgroundColor = `rgb(${color4.r}, ${color4.g}, ${color4.b})`;
+                backgroundColor = 4;
             } else {
-                backgroundColor = `rgb(${color1.r}, ${color1.g}, ${color1.b})`;
+                backgroundColor = 1;
             }
 
             return {
@@ -89,77 +89,48 @@ class HarmonyConfetti extends Component {
         const color3Style = { backgroundColor: `rgb(${color3.r}, ${color3.g}, ${color3.b})` };
         const color4Style = { backgroundColor: `rgb(${color4.r}, ${color4.g}, ${color4.b})` };
 
-        const smallBoxes = this.state.smallBoxPositions.map((boxPosition, i) => {
-            const style = assign({}, color1Style, boxPosition);
-
-            return (
-                <div
-                    className="HarmonyConfetti_smallBox HarmonyConfetti_smallBox--12 position-absolute"
-                    style={style}
-                    key={i}
-                />
-            );
-        })
-
         return (
             <div className="full-screen overflow-hidden" style={mainBgStyle}>
                 <CornerMenu colorLabels={['background color', 'color 1', 'color 2', 'color 3', 'color 4']}>
-                    <button onClick={this.randomizeSmallBoxes}>randomize confetti position (k)</button>
+                    <button onClick={this.randomizeSmallBoxes}>randomize confetti (k)</button>
                     <br/>
                 </CornerMenu>
 
                 {/* bar one */}
-                <div className="HarmonyConfetti_mainBar full-width position-relative">
-
-                    {smallBoxes.slice(0,30)}
-
-                    <div
-                        className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--first position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--second position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--third position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--fourth position-absolute"
-                        style={color1Style}
-                    />
-
-                    {smallBoxes.slice(30,60)}
-
-                    <div
-                        className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--first position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--second position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--third position-absolute"
-                        style={color1Style}
-                    />
-                    <div
-                        className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--fourth position-absolute"
-                        style={color1Style}
-                    />
-
-                    {smallBoxes.slice(60)}
-                </div>
+                <HarmonyConfettiMainBar
+                    smallBoxProperties={this.state.smallBoxProperties}
+                    color1Style={color1Style}
+                    color2Style={color2Style}
+                    color3Style={color3Style}
+                    color4Style={color4Style}
+                />
 
                 {/* bar two */}
-                <div className="HarmonyConfetti_mainBar full-width position-relative"></div>
+                <HarmonyConfettiMainBar
+                    smallBoxProperties={this.state.smallBoxProperties}
+                    color1Style={color2Style}
+                    color2Style={color3Style}
+                    color3Style={color4Style}
+                    color4Style={color1Style}
+                />
 
                 {/* bar three */}
-                <div className="HarmonyConfetti_mainBar full-width position-relative"></div>
+                <HarmonyConfettiMainBar
+                    smallBoxProperties={this.state.smallBoxProperties}
+                    color1Style={color3Style}
+                    color2Style={color4Style}
+                    color3Style={color1Style}
+                    color4Style={color2Style}
+                />
 
                 {/* bar four */}
-                <div className="HarmonyConfetti_mainBar full-width position-relative"></div>
+                <HarmonyConfettiMainBar
+                    smallBoxProperties={this.state.smallBoxProperties}
+                    color1Style={color4Style}
+                    color2Style={color1Style}
+                    color3Style={color2Style}
+                    color4Style={color3Style}
+                />
             </div>
         );
     }
@@ -172,3 +143,68 @@ HarmonyConfetti.propTypes = {
 HarmonyConfetti = connect(mapStateToProps)(HarmonyConfetti);
 
 export default HarmonyConfetti;
+
+function HarmonyConfettiMainBar(props) {
+    const { smallBoxProperties, color1Style } = props;
+
+    const smallBoxes = smallBoxProperties.map((boxProperties, i) => {
+        const style = {
+            top: boxProperties.top,
+            left: boxProperties.left,
+            backgroundColor: props[`color${boxProperties.backgroundColor}Style`]['backgroundColor']
+        };
+
+        return (
+            <div
+                className="HarmonyConfetti_smallBox HarmonyConfetti_smallBox--12 position-absolute"
+                style={style}
+                key={i}
+            />
+        );
+    })
+
+    return (
+        <div className="HarmonyConfetti_mainBar full-width position-relative">
+
+            {smallBoxes.slice(0,30)}
+
+            <div
+                className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--first position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--second position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--third position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_smallBar HarmonyConfetti_smallBar--fourth position-absolute"
+                style={color1Style}
+            />
+
+            {smallBoxes.slice(30,60)}
+
+            <div
+                className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--first position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--second position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--third position-absolute"
+                style={color1Style}
+            />
+            <div
+                className="HarmonyConfetti_largeBox HarmonyConfetti_largeBox--fourth position-absolute"
+                style={color1Style}
+            />
+
+            {smallBoxes.slice(60)}
+        </div>
+    );
+}
