964430b988452b08b4afe13fd89d91870efb87e5
Russ Rinzler <russ.rinzler@gmail.com>
2018-03-20 09:09:46 -0700

    change optical mixture thumb from stripes to blocks

--- src/components/Home/thumbnails/OpticalMixtureThumb.jsx
+++ src/components/Home/thumbnails/OpticalMixtureThumb.jsx
@@ -3,18 +3,17 @@ import HomeLink from '../../HomeLink';
 import PropTypes from 'prop-types';
 
 export default function OpticalMixtureThumb({ color0Rgb, color1Rgb }) {
+    const bgColor1 = `rgb(${color1Rgb.r}, ${color1Rgb.g}, ${color1Rgb.b})`;
+
     const opticalMixtureBackground = {
-        background: `repeating-linear-gradient(90deg,
-            rgb(${color0Rgb.r},
-            ${color0Rgb.g}, ${color0Rgb.b}),
-            rgb(${color0Rgb.r}, ${color0Rgb.g},
-            ${color0Rgb.b}) 2px,
-            rgb(${color1Rgb.r},
-            ${color1Rgb.g},
-            ${color1Rgb.b}) 2px,
-            rgb(${color1Rgb.r},
-            ${color1Rgb.g},
-            ${color1Rgb.b}) 4px )`
+        backgroundColor: `rgb(${color0Rgb.r}, ${color0Rgb.g}, ${color0Rgb.b})`,
+        backgroundImage:
+            `linear-gradient(45deg, ${bgColor1} 25%, transparent 25%),
+            linear-gradient(-45deg, ${bgColor1} 25%, transparent 25%),
+            linear-gradient(45deg, transparent 75%, ${bgColor1} 75%),
+            linear-gradient(-45deg, transparent 75%, ${bgColor1} 75%)`,
+        backgroundSize: '8px 8px',
+        backgroundPosition: '0 0, 0 4px, 4px -4px, -4px 0px'
     };
 
     return (
